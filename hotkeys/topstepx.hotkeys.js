d4facc63cc291793fcf85dbd5d4c157ece17432b
cargocult <cargocultstacks@gmail.com>
2024-04-01 20:33:17 -0500

    set quantity

--- hotkeys/topstepx.hotkeys.js
+++ hotkeys/topstepx.hotkeys.js
@@ -1,16 +1,25 @@
-function buttonClickBuySellCommon(buttonTextToSearch, quantity)
+function buttonClickCommon(buttonTextToSearch)
 {
-    console.log(buttonTextToSearch + ' Quantity ' + quantity + ' Hotkey pressed');
+    var button = [...document.querySelector('#orderCardTab').querySelectorAll('button')].filter(button => button.innerHTML.toLowerCase().startsWith(buttonTextToSearch))[0];
+    if(button == null) { console.log('Unable to locate button with text: ' + buttonTextToSearch); return; }
+    button.click();
+}
+
+function setQuantityCommon(quantity)
+{
     var quantity_input = document.querySelector('div[class^=ordercard_order] input[type=number]')
     if(quantity_input == null) { console.log('Unable to locate Quantity Edit Box'); return; }
     quantity_input.value = quantity;
     quantity_input.dispatchEvent(new Event('change', {bubbles: true}));
-    
-    var button = [...document.querySelector('#orderCardTab').querySelectorAll('button')].filter(button => button.innerHTML.toLowerCase().startsWith(buttonTextToSearch))[0];
-    if(button == null) { console.log('Unable to locate button with text: ' + buttonTextToSearch); return; }
-    button.click();
-
 }
+
+function buttonClickBuySellCommon(buttonTextToSearch, quantity)
+{
+    console.log(buttonTextToSearch + ' Quantity ' + quantity + ' Hotkey pressed');
+    setQuantity(quantity);
+    buttonClickCommon(buttonTextToSearch);
+}
+
 function buttonClickMarketQuantity(buySell, quantity)
 {
     var buttonTextToSearch = buySell ? 'buy' : 'sell';
@@ -22,3 +31,29 @@ function buttonClickJoinQuantity(buySell, quantity)
     var buttonTextToSearch = buySell ? 'join bid' : 'join ask';
     buttonClickBuySellCommon(buttonTextToSearch, quantity);
 }
+
+function buttonClickClosePosition()
+{
+    buttonClickCommon('close position');
+}
+
+function buttonClickReversePosition()
+{
+    buttonClickCommon('reverse position');
+}
+
+function buttonClickFlattenAll()
+{
+    buttonClickCommon('flatten all');
+}
+
+function buttonClickCancelAll()
+{
+    buttonClickCommon('cancel all');
+}
+
+function setQuantity(quantity)
+{
+    setQuantityCommon(quantity);
+}
+
