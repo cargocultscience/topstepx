9fb528e81b435623bd91151d3f6df17373e1c4c4
cargocult <cargocultstacks@gmail.com>
2024-04-01 20:04:40 -0500

    button click

--- hotkeys/topstepx.hotkeys.js
+++ hotkeys/topstepx.hotkeys.js
@@ -1,11 +1,13 @@
-function buttonClickMarketBuyQuantity(quantity)
+function buttonClickMarketQuantity(buySell, quantity)
 {
-    console.log("Buy Quantity 1 Hotkey pressed");
+    var buttonTextToSearch = buySell ? 'buy' : 'sell';
+    console.log(buttonTextToSearch + ' Quantity ' + quantity + ' Hotkey pressed');
     var quantity_input = document.querySelector('div[class^=ordercard_order] input[type=number]')
-    if(quantity_input == null) { console.log("Unable to locate Quantity Edit Box"); return; }
+    if(quantity_input == null) { console.log('Unable to locate Quantity Edit Box'); return; }
     quantity_input.value = quantity;
     quantity_input.dispatchEvent(new Event('change', {bubbles: true}));
-    var button = [...document.querySelector('#orderCardTab').querySelectorAll('button')].filter(button => button.innerHTML.toLowerCase().startsWith("buy"))[0];
-    if(button == null) { console.log("Unable to locate button"); return; }
+    
+    var button = [...document.querySelector('#orderCardTab').querySelectorAll('button')].filter(button => button.innerHTML.toLowerCase().startsWith(buttonTextToSearch))[0];
+    if(button == null) { console.log('Unable to locate button with text: ' + buttonTextToSearch); return; }
     button.click();
 }
