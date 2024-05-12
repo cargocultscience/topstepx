15d52b1dd9f035445c3124624d74770e7d570081
cargocult <cargocultstacks@gmail.com>
2024-05-12 12:42:15 -0500

    overrides

---
+++ hotkeys/topstepx.hotkeys.cjs.override.test.js
@@ -0,0 +1,4 @@
+var hotkeys = [
+    {"keys" : ["alt", "ctrl", "keyb"], "f" : () => buttonClickBuySellMarket(true)},
+    {"keys" : ["alt", "ctrl", "keys"], "f" : () => buttonClickBuySellMarket(false)},
+];
