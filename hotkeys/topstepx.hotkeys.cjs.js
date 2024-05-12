da79ae803a60744b024546d216c14fe8e6f97c82
cargocult <cargocultstacks@gmail.com>
2024-05-12 11:30:46 -0500

    cjs

---
+++ hotkeys/topstepx.hotkeys.cjs.js
@@ -0,0 +1,171 @@
+function registerHotkeys(hotkeys) {
+    var hotkeysDict = {}
+    hotkeys.forEach((m) => hotkeysDict[m["keys"].sort().join()] = m["f"])
+
+    document.addEventListener('keydown',handleKeyDown);
+    function handleKeyDown(event) {
+        let eventKeySet = new Set();
+        if(event.shiftKey)
+        {
+            eventKeySet.add("shift");
+        }
+        if(event.altKey)
+        {
+            eventKeySet.add("alt");
+        }
+        if(event.ctrlKey)
+        {
+            eventKeySet.add("ctrl");
+        }
+        eventKeySet.add(event.code.toLowerCase());
+        eventKey = Array.from(eventKeySet).sort().join();
+        
+        console.log(event, eventKeySet, eventKey);
+        if(eventKey in hotkeysDict)
+        {
+            console.log("Firing hotkey: " + eventKey)
+            hotkeysDict[eventKey]();
+        }
+    }
+}
+
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function buttonClickCommon(buttonTextToSearch)
+{
+    var button = [...document.querySelector('[id^=orderCardTab]').querySelectorAll('button')].filter(button => button.innerHTML.toLowerCase().startsWith(buttonTextToSearch))[0];
+    if(button == null) { console.log('Unable to locate button with text: ' + buttonTextToSearch); return; }
+    button.click();
+}
+
+function setQuantityCommon(quantity)
+{
+    var quantity_input = document.querySelector('div[class^=ordercard_order] input[type=number]')
+    if(quantity_input == null) { console.log('Unable to locate Quantity Edit Box'); return; }
+    quantity_input.value = quantity;
+    quantity_input.dispatchEvent(new Event('change', {bubbles: true}));
+}
+
+
+function buttonClickBuySellQuantityCommon(buttonTextToSearch, quantity)
+{
+    setQuantity(quantity);
+    buttonClickCommon(buttonTextToSearch);
+}
+
+function buttonClickBuySellMarketQuantity(buySell, quantity)
+{
+    var buttonTextToSearch = buySell ? 'buy' : 'sell';
+    console.log(buttonTextToSearch + ' Quantity ' + quantity + ' hotkey pressed');  
+    buttonClickBuySellQuantityCommon(buttonTextToSearch, quantity);
+}
+
+function buttonClickBuySellJoinQuantity(buySell, quantity)
+{
+    var buttonTextToSearch = buySell ? 'join bid' : 'join ask';
+    console.log(buttonTextToSearch + ' Quantity ' + quantity + ' hotkey pressed');
+    buttonClickBuySellQuantityCommon(buttonTextToSearch, quantity);
+}
+
+function buttonClickBuySellMarket(buySell)
+{
+    var buttonTextToSearch = buySell ? 'buy' : 'sell';
+    console.log(buttonTextToSearch + ' hotkey pressed');
+    buttonClickCommon(buttonTextToSearch);
+}
+
+function buttonClickBuySellJoin(buySell)
+{
+    var buttonTextToSearch = buySell ? 'join bid' : 'join ask';
+    console.log(buttonTextToSearch + ' hotkey pressed');
+    buttonClickCommon(buttonTextToSearch);
+}
+
+function buttonClickClosePosition()
+{
+    console.log('Close Position hotkey pressed');
+    buttonClickCommon('close position');
+}
+
+function buttonClickReversePosition()
+{
+    console.log('Reverse Position hotkey pressed');
+    buttonClickCommon('reverse position');
+}
+
+function buttonClickCancelOrders()
+{
+    console.log('Cancel Orders hotkey pressed');
+    buttonClickCommon('cancel orders');
+}
+
+function buttonClickFlattenAll()
+{
+    console.log('Flatten All hotkey pressed');
+    buttonClickCommon('flatten all');
+}
+
+function buttonClickCancelAll()
+{
+    console.log('Cancel All hotkey pressed');
+    buttonClickCommon('cancel all');
+}
+
+function buttonClickStopBreakEven()
+{
+    console.log('Stop Break Even hotkey pressed');
+    buttonClickCommon('b/e');
+}
+
+function setQuantity(quantity)
+{
+    console.log('Set Quantity ' + quantity + ' hotkey pressed');
+    setQuantityCommon(quantity);
+}
+
+async function setContract(contract, delayMilliseconds)
+{
+    var input_div = [...document.querySelectorAll('div[class^=MuiInputBase-root')].filter(d => d.innerText.toLowerCase().startsWith('contract'))[0]
+    if(input_div == null)
+    {
+        console.log('unable to find input div for contract section');
+        return;
+    }
+
+    input_div.dispatchEvent(new Event('click', { bubbles : true }));
+    await sleep(delayMilliseconds);
+    var list_item = [...document.querySelectorAll('li')].filter(d => d.innerText.toLowerCase().startsWith(contract.toLowerCase()))[0]
+    if(list_item == null)
+    {
+        console.log('unable to find li element for contract ' + contract);
+    }
+    else
+    {
+        list_item.dispatchEvent(new Event('click', { bubbles: true }));
+    }
+    input_div.dispatchEvent(new Event('click', { bubbles : true }));
+}
+
+async function setAccount(account)
+{
+	var input_div = document.querySelector('div[class^=ordercard_account]').querySelector('div[role=combobox]')
+    if(input_div == null)
+    {
+        console.log('unable to find input div for contract section');
+        return;
+    }
+	input_div.dispatchEvent(new KeyboardEvent('keydown', {key: 'ArrowUp', bubbles: true}));
+    await sleep(10);
+	var account_li = [...document.querySelectorAll('li')].filter(d => d.innerText.toLowerCase().includes(account.toLowerCase()))[0]
+    if(account_li == null)
+    {
+        console.log('unable to find li element for account ' + account);
+    }
+    else
+    {
+        account_li.dispatchEvent(new Event('click', { bubbles: true }));
+    }
+}
