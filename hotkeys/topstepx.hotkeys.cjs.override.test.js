8841059371b67cb782985bb170a71e8edfe714ec
cargocult <cargocultstacks@gmail.com>
2024-05-27 14:47:49 -0500

    delete cjs files

--- hotkeys/topstepx.hotkeys.cjs.override.test.js
+++
@@ -1,4 +0,0 @@
-var hotkeys = [
-    {"keys" : ["alt", "ctrl", "keyb"], "f" : () => buttonClickBuySellMarket(true)},
-    {"keys" : ["alt", "ctrl", "keys"], "f" : () => buttonClickBuySellMarket(false)},
-];
